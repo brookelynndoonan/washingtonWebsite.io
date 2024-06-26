e87fb9b03cc52d776967e540bb80fafe53f0e5f8
brookelynndoonan <brookelynn.doonan@mykenzie.snhu.edu>
2024-06-25 23:18:46 -0600

    updated javascript to change to red, if incorrect and has you continue. If correct, allows you to proceed. Post link is fake.

--- javascript.js
+++ javascript.js
@@ -8,9 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (email !== confirmEmail) {
             alert("Your email does not match, please try again.");
+            document.getElementById('confirmEmail').style.color = "crimson";
             event.preventDefault(); 
             return false; 
         }
+        if (email == confirmEmail)
+            alert("Your emails match, please continue!");
+        document.getElementById('confirmEmail').style.color = "green";
         return true;
     });
 });
